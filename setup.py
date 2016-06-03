0ff6cab7fd6952365506c0dc4510bd93f69defa9
Tim Heithecker <tim.heithecker@gmail.com>
2016-06-03 16:18:34 +0200

    dummy tag for datacite-fork

--- setup.py
+++ setup.py
@@ -81,13 +81,13 @@ setup(
         "SQLAlchemy==1.0.12",
         "celery==3.1.23",
         "pypandoc==1.1.3",
-        "datacite>=0.2.1",
+        "datacite>=0.2.999",
         "PyYAML==3.11",
         "eventlet==0.19.0",
 
     ],
      dependency_links=[
-         "https://github.com/inveniosoftware/datacite/archive/master.zip#egg=datacite-0.2.1",
+         "https://github.com/inveniosoftware/datacite/archive/master.zip#egg=datacite-0.2.999",
      ],
 
     # List additional groups of dependencies here (e.g. development
